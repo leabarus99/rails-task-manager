9b2346e6a28ea946bc5101692e35ca5a4164ef5f
Lea Barus <lea.barus31@gmail.com>
2022-05-31 14:03:53 +0200

    task management

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :restaurants
+  # read all tasks
+  get 'tasks', to: 'tasks#index', as: :tasks
+  # # Add a task
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  # # update a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  # # delete a task
+  delete 'tasks/:id', to: 'tasks#destroy'
+
+  # # One task detailed
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
